0303cf4b956ac4c1aa25838d46681b79ed2494a3
Navneet Yadav <navneet@kgpian.iitkgp.ac.in>
2023-06-11 13:46:36 +0530

    gf

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Restaurant from "../Components/Restaurant";
+import Restaurantt from "../Components/Restaurantt";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
@@ -7,7 +7,7 @@ function Home(){
     return(
         <>
             <Header/>
-            <Restaurant />
+            <Restaurantt />
             <Footer/>
         </>
     )
