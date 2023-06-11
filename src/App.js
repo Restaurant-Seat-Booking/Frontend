61dc96d15cec9d55856e3532bcf53b1d97537b8f
Navneet Yadav <navneet@kgpian.iitkgp.ac.in>
2023-06-11 11:07:59 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header'
+import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <Header />
+      <Login />
     </>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -7,13 +7,13 @@ export default function Header() {
         <nav class="navbar navbar-expand-lg bg-body-tertiary ">
             <div class="container">
                 <a class="navbar-brand" href="#">
-                    <img src='https://source.unsplash.com/1600*900/?nature' width="30" height="24" class="d-inline-block align-text-top"></img>
+                    <img src='' width="30" height="24" class="d-inline-block align-text-top"></img>
                     Resto</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
-                    <ul class="navbar-nav  me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav  ms-auto">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
@@ -25,11 +25,31 @@ export default function Header() {
                         </li>
                     </ul>
                 </div>
-                {/* <div  >
-                    <button type="button" class="btn btn-outline-success ml-auto">Log In</button>
+                <div class="mx-2" >
+                    <button type="button" class="btn btn-outline-success ml-auto ">Log In</button>
+                    
                     <button type="button" class="btn btn-danger ml-2">Sign Up</button>
-                </div> */}
+                </div>
             </div>
         </nav>
+        
+        // <!-- Login Modal -->
+        // <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+        //   <div class="modal-dialog">
+        //     <div class="modal-content">
+        //       <div class="modal-header">
+        //         <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        //         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        //       </div>
+        //       <div class="modal-body">
+        //       <p>Modal body text goes here.</p>
+        //       </div>
+        //       <div class="modal-footer">
+        //         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        //         <button type="button" class="btn btn-primary">Save changes</button>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
     )
 }
