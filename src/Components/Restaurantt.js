6bf9c8fd7eba03c0f872aa51225474ca91d68adf
Navneet Yadav <navneet@kgpian.iitkgp.ac.in>
2023-06-12 00:30:46 +0530

    s

--- src/Components/Restaurantt.js
+++ src/Components/Restaurantt.js
@@ -104,7 +104,7 @@ const Carousel = () => {
                         <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
                     </div>
                     <div class="col-md-5">
-                    <img src="./images/res1.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="300" alt="Slide 1"  />
+                    <img src="./images/res1.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" style={{ height: '500px', width: '400px' }} alt="Slide 1"  />
                         {/* <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg> */}
                     </div>
                 </div>
@@ -117,7 +117,7 @@ const Carousel = () => {
                         <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
                     </div>
                     <div class="col-md-5 order-md-1">
-                    <img src="./images/res2.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" alt="Slide 2"  />
+                    <img src="./images/res2.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" style={{ height: '500px', width: '400px' }} alt="Slide 2"  />
                         {/* <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg> */}
                     </div>
                 </div>
@@ -130,7 +130,7 @@ const Carousel = () => {
                         <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
                     </div>
                     <div class="col-md-5">
-                    <img src="./images/res3.jpg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" alt="Slide 3"  />
+                    <img src="./images/res3.jpg" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" style={{ height: '500px', width: '400px' }} alt="Slide 3" />
                         {/* <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"></rect><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg> */}
                     </div>
                 </div>
