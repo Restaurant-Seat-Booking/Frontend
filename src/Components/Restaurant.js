f84f5e46508dbe1e62f1ae8eb3e37d3f5ae39cbd
Navneet Yadav <navneet@kgpian.iitkgp.ac.in>
2023-06-11 12:54:38 +0530

    s

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -9,7 +9,7 @@ function FeaturedPost() {
           <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm position-relative" style={{ height: "500px" }}>
             <div className="col p-4 d-flex flex-column position-static">
               <strong className="d-inline-block mb-2 text-primary-emphasis">World</strong>
-              <h3 className="mb-0">Featured post</h3>
+              <h3 className="mb-0">Restaurant Name</h3>
               <div className="mb-1 text-body-secondary">Nov 12</div>
               <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
               <a href="#" className="icon-link gap-1 icon-link-hover stretched-link">
