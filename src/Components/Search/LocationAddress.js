19ad73145603cd59db192d0f2a4046bf13adc470
Ved Prakash <vedprakash182001@gmail.com>
2023-06-16 23:23:26 +0530

    s

---
+++ src/Components/Search/LocationAddress.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+const LocationAddress = ({ onLocationChange }) => {
+  const [address, setAddress] = useState('');
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  const handleGetAddress = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lng = position.coords.longitude;
+          setLatitude(lat);
+          setLongitude(lng);
+          onLocationChange(lat, lng); // Call onLocationChange with latitude and longitude
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    } else {
+      console.error('Geolocation is not supported by your browser');
+    }
+  };
+
+  return (
+    <div>
+      <p>Your Address: {address}</p>
+      <button type="button" onClick={handleGetAddress}>
+        Get My Address
+      </button>
+    </div>
+  );
+};
+
+export default LocationAddress;
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,20 +1,45 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import SearchBar from './SearchBar';
 import RestaurantList from './RestaurantList';
+import LocationAddress from './LocationAddress';
 
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState('');
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
 
   const handleSearch = (query) => {
     setSearchQuery(query);
-
-    // Send the search query to the backend
-    fetch('http://localhost:7000/api/search/getrestaurant', {
+    console.log(`Query: ${query}`);
+    // Send the search query, latitude, and longitude to the backend
+    fetch('http://localhost:7000/api/search/byName', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ query }),
+      body: JSON.stringify({query}),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data); // Log the response from the backend
+      })
+      .catch((error) => {
+        console.error(error); // Handle any errors
+      });
+  };
+
+
+  const handleLocationChange = (lat, lng) => {
+    setLatitude(lat);
+    setLongitude(lng);
+    console.log(`Latitude: ${lat}`);
+    console.log(`Longitude: ${lng}`);
+    fetch('http://localhost:7000/api/search/byLocation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ lat, lng }),
     })
       .then((response) => response.text())
       .then((data) => {
@@ -42,13 +67,12 @@ const Search = () => {
   ];
 
   return (
-    <>
-      <div className="container">
-        <SearchBar onSearch={handleSearch} />
-        <h1 className="text-center">Restaurant List</h1>
-        <RestaurantList restaurants={restaurants} />
-      </div>
-    </>
+    <div className="container">
+      <SearchBar onSearch={handleSearch} />
+      <LocationAddress onLocationChange={handleLocationChange} />
+      <h1 className="text-center">Restaurant List</h1>
+      <RestaurantList restaurants={restaurants} />
+    </div>
   );
 };
 
