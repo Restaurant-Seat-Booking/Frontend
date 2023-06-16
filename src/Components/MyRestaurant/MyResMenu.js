0638c31d75333ab24a64d91c9658ee18e036e2fa
Ved Prakash <vedprakash182001@gmail.com>
2023-06-16 13:32:40 +0530

    s

--- src/Components/MyRestaurant/MyResMenu.js
+++ src/Components/MyRestaurant/MyResMenu.js
@@ -1,23 +1,64 @@
-import React from 'react';
-import MyResMenuDis from './MyResMenuDis'
+// ResMenu.js
+import React, { useEffect, useState } from 'react';
+import MyResMenuDis from './MyResMenuDis';
 
 const MyResMenu = () => {
-  const menuItems = [
-    {
-      name: 'Item 1',
-      ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
-    },
-    {
-      name: 'Item 2',
-      ingredients: ['Ingredient 4', 'Ingredient 5', 'Ingredient 6'],
-    },
-    // Add more menu items as needed
-  ];
+  const [items, setItems] = useState([]);
+  const email = localStorage.getItem('userId');
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch('http://localhost:7000/api/about/about', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email }),
+        });
+        if (!response.ok) {
+          throw new Error('Failed to fetch orders');
+        }
+        const data = await response.json();
+        const tempdata = data.data;
+        setItems(JSON.parse(tempdata.items));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchOrders();
+  }, [email]);
+
+  const handleSelect = (name, price) => {
+    // Handle selection logic here
+  };
+
+  const handleDeselect = (name) => {
+    // Handle deselection logic here
+  };
+
+  const handleDelete = (name) => {
+    setItems((prevItems) => prevItems.filter((item) => item.name !== name));
+  };
+
+  const handleUpdateItem = (name, updatedItem) => {
+    // Handle item update logic here
+  };
 
   return (
     <div>
-      {menuItems.map((menuItem, index) => (
-        <MyResMenuDis key={index} name={menuItem.name} ingredients={menuItem.ingredients} />
+      {items.map((item, index) => (
+        <MyResMenuDis
+          key={index}
+          name={item.name}
+          price={item.price}
+          ingredients={item.ingredients}
+          onSelect={handleSelect}
+          onDeselect={handleDeselect}
+          onDelete={handleDelete}
+          onUpdateItem={handleUpdateItem}
+        />
       ))}
     </div>
   );
--- src/Components/MyRestaurant/MyResMenuDis.js
+++ src/Components/MyRestaurant/MyResMenuDis.js
@@ -1,31 +1,140 @@
 import React, { useState } from 'react';
 
-const MyResMenuDis = ({ name, ingredients }) => {
+const MyResMenuDis = ({
+  name,
+  price,
+  ingredients,
+  onSelect,
+  onDeselect,
+  onDelete,
+  onUpdateItem,
+}) => {
   const [isSelected, setIsSelected] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
+  const [editedName, setEditedName] = useState(name);
+  const [editedPrice, setEditedPrice] = useState(price);
+  const [editedIngredients, setEditedIngredients] = useState(ingredients);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
   const handleCheckboxClick = (event) => {
     event.stopPropagation();
     setIsSelected(!isSelected);
+    if (!isSelected) {
+      onSelect(name, price);
+    } else {
+      onDeselect(name);
+    }
   };
 
   const handleItemClick = () => {
     setIsExpanded(!isExpanded);
   };
 
+  const handleNameChange = (event) => {
+    setEditedName(event.target.value);
+  };
+
+  const handlePriceChange = (event) => {
+    setEditedPrice(event.target.value);
+  };
+
+  const handleIngredientsChange = (event, index) => {
+    const updatedIngredients = [...editedIngredients];
+    updatedIngredients[index] = event.target.value;
+    setEditedIngredients(updatedIngredients);
+  };
+
+  const handleEditClick = () => {
+    setIsEditModalOpen(true);
+  };
+
+  const handleDelete = () => {
+    onDelete(name);
+    setIsEditModalOpen(false);
+  };
+
+  const handleSave = () => {
+    const updatedItem = {
+      name: editedName,
+      price: editedPrice,
+      ingredients: editedIngredients,
+    };
+    onUpdateItem(name, updatedItem);
+    setIsEditModalOpen(false);
+  };
+
   return (
     <div>
-      <div style={{ display: 'flex', alignItems: 'center' }} onClick={handleItemClick}>
-        <input type="checkbox" checked={isSelected} onClick={handleCheckboxClick} />
-        <div style={{ marginLeft: '5px' }}>{name}</div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+        onClick={handleItemClick}
+      >
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <input type="checkbox" checked={isSelected} onClick={handleCheckboxClick} />
+          <span>{name}</span>
+        </div>
+        <div>
+          <span>{price}</span>
+        </div>
+        <button onClick={handleEditClick}>Edit</button>
       </div>
       {isExpanded && (
         <ul style={{ listStyleType: 'none', marginLeft: '15px', fontSize: 'small' }}>
-          {ingredients.map((ingredient, index) => (
-            <li key={index}>{ingredient}</li>
+          {editedIngredients.map((ingredient, index) => (
+            <li key={index}>
+              <span>
+                {/* value={ingredient}
+                onChange={(event) => handleIngredientsChange(event, index)} */}
+                <p>{ingredient}</p>
+              </span>
+              
+            </li>
           ))}
         </ul>
       )}
+      {isEditModalOpen && (
+        <div
+          style={{
+            position: 'fixed',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <div style={{ backgroundColor: '#fff', padding: '20px', maxWidth: '400px' }}>
+            <h3>Edit Item</h3>
+            <label>Name:</label>
+            <input type="text" value={editedName} onChange={handleNameChange} />
+            <label>Price:</label>
+            <input type="text" value={editedPrice} onChange={handlePriceChange} />
+            <label>Ingredients:</label>
+            <ul>
+              {editedIngredients.map((ingredient, index) => (
+                <li key={index}>
+                  <input
+                    type="text"
+                    value={ingredient}
+                    onChange={(event) => handleIngredientsChange(event, index)}
+                  />
+                </li>
+              ))}
+            </ul>
+            <button onClick={handleSave}>Save</button>
+            <button onClick={handleDelete}>Delete</button>
+            <button onClick={() => setIsEditModalOpen(false)}>Cancel</button>
+          </div>
+        </div>
+      )}
+      <hr style={{ margin: '10px 0' }} />
     </div>
   );
 };
