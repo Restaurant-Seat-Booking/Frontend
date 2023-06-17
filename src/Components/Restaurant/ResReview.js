6611646a6340db617f04cf93ca3b467a3972d814
Ved Prakash <vedprakash182001@gmail.com>
2023-06-17 11:35:12 +0530

    s

--- src/Components/Restaurant/ResReview.js
+++ src/Components/Restaurant/ResReview.js
@@ -1,40 +1,107 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap';
 
 const ReviewsPage = () => {
-  const [reviews, setreviews] = useState([]);
-  const restaurantid = localStorage.getItem('restaurant_id'); // Retrieve user ID from localStorage
+  const [reviews, setReviews] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [reviewText, setReviewText] = useState('');
+  const restaurantid = localStorage.getItem('restaurant_id');
   const email = localStorage.getItem('userId');
 
   useEffect(() => {
-    // console.log(email)
-    const fetchOrders = async () => {
+    const fetchReviews = async () => {
       try {
         const response = await fetch('http://localhost:7000/api/review/review', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ email,restaurantid }), // Send userId in the request body
+          body: JSON.stringify({ email, restaurantid }),
         });
         if (!response.ok) {
-          throw new Error('Failed to fetch orders');
+          throw new Error('Failed to fetch reviews');
         }
         const data = await response.json();
-        const tempdata = data.data
-        setreviews(JSON.parse(tempdata.review));
+        const tempdata = data.data;
+        setReviews(JSON.parse(tempdata.review));
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchOrders();
+    fetchReviews();
   }, [email]);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleReviewChange = (e) => {
+    setReviewText(e.target.value);
+  };
+
+  const saveReview = async () => {
+    try {
+      closeModal();
+      console.log(reviewText)
+      // const response = await fetch('http://localhost:7000/api/review/save', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify({ review: reviewText }),
+      // });
+
+      // if (!response.ok) {
+      //   throw new Error('Failed to save review');
+      // }
+
+      // const newReview = {
+      //   author: 'User',
+      //   review: reviewText,
+      // };
+
+      // setReviews([...reviews, newReview]);
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <section className="reviews-section py-5">
       <Container>
         <h2 className="text-center mb-5">Customer Reviews</h2>
+        <section className="addreview">
+          <Button variant="primary" onClick={openModal}>
+            Add Review
+          </Button>
+          <Modal show={showModal} onHide={closeModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>Add Review</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <textarea
+                className="review-input"
+                placeholder="Enter your review"
+                value={reviewText}
+                onChange={handleReviewChange}
+              />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="success" onClick={saveReview}>
+                Save
+              </Button>
+              <Button variant="danger" onClick={closeModal}>
+                Cancel
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </section>
         <Row>
           {reviews.map((review, index) => (
             <Col md={4} key={index}>
