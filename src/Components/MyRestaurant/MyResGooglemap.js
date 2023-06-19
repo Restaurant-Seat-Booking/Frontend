777e032bf92194e86fda2caffbd923d33143d52b
Navneet Yadav <navneet@kgpian.iitkgp.ac.in>
2023-06-19 11:23:39 +0530

    s

--- src/Components/MyRestaurant/MyResGooglemap.js
+++ src/Components/MyRestaurant/MyResGooglemap.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MyResMapContainer from './MyResMapcontainer';
+import { url } from '../../constants';
 
 const MyResGoogleMap = () => {
+
+  const [name, setname] = useState([]);
+  const [latitude, setlatitude] = useState([]);
+  const [longitude, setlongitude] = useState([]);
+
+  const restaurant_id = localStorage.getItem('restaurant_id')
+  // console.log(restaurant_id)
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch(`${url}/api/about/about`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ restaurant_id }), // Send userId in the request body
+        });
+        if (!response.ok) {
+          throw new Error('Failed to fetch orders');
+        }
+        const data = await response.json();
+        const tempdata = data.data
+        setname(tempdata.name);
+        setlatitude(tempdata.latiude);
+        setlongitude(tempdata.longitude);
+        // setfeatu(JSON.parse(tempdata.features))
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchOrders();
+  }, [restaurant_id]);
+
+
   const restaurants = [
     {
-      id: 1,
-      name: 'IIT Kharapur',
-      lat: 22.3149,
-      lng: 87.3105,
+      id: restaurant_id,
+      name: name,
+      lat: latitude,               //23.3149,
+      lng: longitude               //87.3105,
     },
     // Add more restaurant locations as needed
   ];
